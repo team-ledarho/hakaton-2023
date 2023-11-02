52ecf4f7c5125d442b72efa4e561ec4d2e61b6f1
minkail <mkx.dake@gmail.com>
2023-11-02 21:50:02 +0300

    Добавил авторизацию и propTypes

--- front/src/store/store.js
+++ front/src/store/store.js
@@ -1,13 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { api } from "../services/api";
 import { listenerMiddleware } from "../middleware/auth";
+import authReducer from "../services/slices/auth";
+import { authApi } from "../services/auth/auth";
 
 export const store = configureStore({
-    reducer: {
-        [api.reducerPath]: api.reducer
-    },
-    middleware: (getDefaultMidddleware) =>
+  reducer: {
+    [api.reducerPath]: api.reducer,
+    [authApi.reducerPath]: authApi.reducer,
+    authReducer,
+  },
+  middleware: (getDefaultMidddleware) =>
     getDefaultMidddleware()
       .concat(api.middleware)
       .prepend(listenerMiddleware.middleware),
-})
+});

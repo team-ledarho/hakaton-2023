84cb1d57da7855e4c9fbed77339f5843d7711c93
mxns <mxnsur.hh@gmail.com>
2023-11-04 14:09:29 +0300

    добавил возможность выбора языка

--- front/src/components/language/useLanguage.jsx
+++ front/src/components/language/useLanguage.jsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from 'react';
 import i18next from 'i18next';
 
 const useLanguage = () => {
-  const [language, setLanguage] = useState(localStorage.getItem('locale_fkkfpas') || i18next.language);
+  const [language, setLanguage] = useState(localStorage.getItem('localelang_zarzar') || i18next.language);
 
   useEffect(() => {
     i18next.changeLanguage(language);
-    localStorage.setItem("locale_fkkfpas", language);
+    localStorage.setItem("localelang_zarzar", language);
   }, [language]);
 
   const changeLanguage = (newLanguage) => {
